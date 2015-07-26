4b4831027e964edd9b32ff75ecc10852673e4182
theTinT <racergill17@hotmail.com>
2015-07-26 15:54:37 -0400

    piece class, recreated gameactivity properly, drawpiece class

---
+++ app/src/main/java/ca/waterloo/qworty/DrawPiece.java
@@ -0,0 +1,7 @@
+package ca.waterloo.qworty;
+
+/**
+ * Created by tristan on 2015-07-26.
+ */
+public class DrawPiece {
+}
--- app/src/main/java/ca/waterloo/qworty/Pieces.java
+++ app/src/main/java/ca/waterloo/qworty/Pieces.java
@@ -1,8 +1,36 @@
 package ca.waterloo.qworty;
 
+import android.graphics.Color;
+import android.graphics.Paint;
+
 /**
  * Created by tristan on 2015-07-26.
  */
 public class Pieces {
 
+    private int xPosition;
+    private int yPosition;
+    private String shape;
+    private String size;
+    private String fill;
+    private String color;
+    private Paint paint;
+
+    public Pieces(int xPos, int yPos, String shape, String size, String fill, String color){
+        xPosition = xPos;
+        yPosition = yPos;
+        this.shape = shape;
+        this.size = size;
+        this.fill = fill;
+        this.color = color;
+        paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.parseColor(color));
+    }
+    public void setX(int x){
+        xPosition = x;
+    }
+    public void setY(int y){
+        yPosition = y;
+    }
 }
