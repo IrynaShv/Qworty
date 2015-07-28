6fb1138ce5e70bc8b0deb89f76ce6ef27cf182dc
IrynaShv <iryna.shvydchenko@gmail.com>
2015-07-27 23:02:47 -0400

    working with PieceView

--- app/src/main/java/ca/waterloo/qworty/PieceView.java
+++ app/src/main/java/ca/waterloo/qworty/PieceView.java
@@ -2,26 +2,35 @@ package ca.waterloo.qworty;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.util.DisplayMetrics;
 import android.view.View;
 
 public class PieceView extends View {
 
     private Pieces pieces;
-    private int width;
-    private int height;
-    private int pieceWidth;
-
+    private int width, height, viewWidth, viewHeight, smallPieceRad, bigPieceRad, widthCent, heighCent, screenHeightWOView;
+    private Context gameContext;
 
     public PieceView(Context context, Pieces newPieces) {
         super(context);
         pieces = newPieces;
+        gameContext = context;
     }
 
     protected void OnDraw(Canvas canvas){
-        width = this.getWidth();
-        height = this.getHeight();
+        viewWidth = this.getWidth();
+        viewHeight = this.getHeight();
+        width = viewWidth/8;
+        height = viewHeight/2;
+        DisplayMetrics metrics = gameContext.getResources().getDisplayMetrics();
+        screenHeightWOView = metrics.heightPixels - viewHeight;
+        smallPieceRad = viewWidth/2;
+        bigPieceRad = viewWidth*(3/4);
 
-        //for(int i = 0; i<)
+        for(int i = 0; i< pieces.getPieces().length; i++)
+        {
+
+        }
 
     }
 }
